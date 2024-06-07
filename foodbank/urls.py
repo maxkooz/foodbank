587e790a41e5b660c730aee172964f56024dc421
Nafis Hossain <nafishss03@gmail.com>
2024-06-07 13:56:09 -0400

    added login & homepage views

--- foodbank/urls.py
+++ foodbank/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from .views import home_view
+from .views import home_view, volunteer_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('main_page/', LoginView.as_view(template_name='main_page.html', redirect_authenticated_user=True), name='main_page'),
+    path('volunteer/', volunteer_view, name='volunteer'),
 ]
