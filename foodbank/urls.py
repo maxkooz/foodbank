edc3cf95f0dbf86bf12c152a9a7c52b2be4ba667
maxkooz <itsmkooz@gmail.com>
2024-06-07 15:24:14 -0400

    added login, logout, and sign up

--- foodbank/urls.py
+++ foodbank/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from .views import home_view
+from . import views
 
+app_name = 'foodbank'
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('main_page/', LoginView.as_view(template_name='main_page.html', redirect_authenticated_user=True), name='main_page'),
+    path('', views.home_view, name='home'),
+    path('sign_up/', views.sign_up_view, name='sign_up'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('main_page/', views.main_page_view, name='main_page'),
 ]
--- foodbank/views.py
+++ foodbank/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
-# Create your views here.iuhiuhi
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def home_view(request):
     return render(request, 'home.html')
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+
+        return redirect(reverse('foodbank:main_page'))
+    else:
+        return render(request, 'home.html', {"error_msg": "Login Failed"})
+    
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('foodbank:main_page'))
+
+def sign_up_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    first_name = request.POST["first_name"]
+    last_name = request.POST["last_name"]
+    email = request.POST["email"]
+
+    user = User.objects.create_user(username, email=email, password=password)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.save()
+
+    return redirect(reverse('foodbank:main_page'))
+
+def main_page_view(request):
+    if not request.user.is_authenticated:
+        return redirect(reverse('foodbank:home'))
+
+    return render(request, 'main_page.html')
